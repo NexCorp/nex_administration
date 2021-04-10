19572af83b6e6811d4ad98c67c41193b760db260
Otunga <lukemcsalley@hotmail.co.uk>
2021-04-10 17:28:32 +0200

    whoops

--- nex_administration/config.lua
+++
@@ -1,9 +0,0 @@
-Config = {}
-
-
-Config.AdminGroups  = {"admin"}
-Config.BanFormat    = "Banned!\n==============================\nReason: %s\n==============================\nExpires: %s\nBanned for: %s (Ban ID: #%s)\nDiscord: ---"
-
-Config.UseSreenshotBasic = false
-
-Config.SendScreeshotToDiscord = false
